511233d84cd94279e0255731868eaff8165ebeb8
Haazem66 <61153924+Haazem66@users.noreply.github.com>
2023-10-02 22:58:41 +0300

    mongodb

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname , 'public')))
 
 app.use((req,res,next) => {
     User.findById('651a0c8639cb2ed6e6cd7f78').then(user => {
-        req.user = user;
+        req.user = new User(user.name , user.email , user.cart , user._id);
         next();
     }).catch(err => console.log(err))
 })
