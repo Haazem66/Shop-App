c40fd76cd12c4e3df54221c4bf71991f99f0f209
Haazem66 <61153924+Haazem66@users.noreply.github.com>
2023-10-11 03:39:42 +0300

    Valid

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,7 @@
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const User = require("../models/user");
-
-
+const {validationResult} = require('express-validator');
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
@@ -77,6 +76,14 @@ exports.postSignup = (req , res ,next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors =  validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array()[0].msg
+        });
+    }
     User.findOne({ email: email })
         .then(userDoc => {
             if (userDoc) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator')
 const authController = require('../controllers/auth');
 
 router.get('/login' , authController.getLogin);
 router.get('/signup' , authController.getSignup);
 router.post('/login' , authController.postLogin);
-router.post('/signup' , authController.postSignup);
+router.post('/signup', check('email').isEmail().withMessage('Please Enter a valid Email')
+.custom((value , {req}) => {
+    if (value === 'test@test.com'){
+        console.log('Error');
+        throw new Error('This email is forbidden');
+    }
+    return true;
+}) , authController.postSignup);
 router.post('/logout' , authController.postLogout);
 router.get('/reset' , authController.getReset);
 router.post('/reset' , authController.postReset);
